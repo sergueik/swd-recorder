0a4ce3e0b12f304ca89580ed9f8b27c47b881bd6
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-07-31 15:41:34 -0400

    added MVP placeholder

---
+++ SwdPageRecorder/SwdPageRecorder.UI/SwdMain/Panels/CapabilitiesDataGrid.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SwdPageRecorder.UI
+{
+    public partial class CapabilitiesDataGridView : UserControl, IView
+    {
+        public CapabilitiesDataGridPresenter Presenter { get; private set; }
+        public CapabilitiesDataGridView()
+        {
+            InitializeComponent();
+            Presenter = Presenters.CapabilitiesDataGridPresenter;
+            Presenter.InitWithView(this);
+        }
+
+    }
+}
